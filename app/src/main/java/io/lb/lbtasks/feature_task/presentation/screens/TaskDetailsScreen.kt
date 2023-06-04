d9441c598866871eb4d3702cc3af430ab909e90e
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2023-06-04 01:28:40 -0300

    Update TaskDetailsScreen.kt

--- app/src/main/java/io/lb/lbtasks/feature_task/presentation/screens/TaskDetailsScreen.kt
+++ app/src/main/java/io/lb/lbtasks/feature_task/presentation/screens/TaskDetailsScreen.kt
@@ -2,7 +2,6 @@ package io.lb.lbtasks.feature_task.presentation.screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
